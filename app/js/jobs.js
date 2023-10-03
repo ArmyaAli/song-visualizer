1ccd71fb423e4704f4a4b24c955f618a19f57134
Ali Umar <ali.umar.work@gmail.com>
2023-10-03 11:15:20 -0400

    Group view data together

--- app/js/jobs.js
+++ app/js/jobs.js
@@ -1,21 +1,21 @@
-import { canvasContext, canvasHeight, canvasWidth, m__FPS } from "./state.js";
+import { canvasContext, ViewState, m__FPS } from "./state.js";
 import { audioGraph, audioState } from "./state.js";
 
 export const drawVerticalBars = () => {
-    canvasContext.clearRect(0, 0, canvasWidth, canvasHeight); // clear canvas
+    canvasContext.clearRect(0, 0, ViewState.canvasWidth, ViewState.canvasHeight); // clear canvas
     audioGraph.analyserNode.getByteFrequencyData(audioState.dataArray);
     canvasContext.fillStyle = `rgb(0,0,0)`;
-    canvasContext.fillRect(0, 0, canvasWidth, canvasHeight);
+    canvasContext.fillRect(0, 0, ViewState.canvasWidth, ViewState.canvasHeight);
     canvasContext.fillStyle = `rgb(255,255,255)`;
 
-    const barWidth = (canvasWidth / audioState.bufferLength) * 2.5;
+    const barWidth = (ViewState.canvasWidth / audioState.bufferLength) * 2.5;
     let barHeight;
     let x = 0;
 
     for (let i = 0; i < audioState.bufferLength; i++) {
         barHeight = audioState.dataArray[i];
         canvasContext.fillStyle = `rgb(${barHeight + 100}, 50, 50)`;
-        let y = canvasHeight - barHeight / 2;
+        let y = ViewState.canvasHeight - barHeight / 2;
         canvasContext.fillRect(x, y, barWidth, barHeight);
         x += barWidth + 1;
     }
@@ -24,10 +24,10 @@ export const drawVerticalBars = () => {
 }
 
 export const drawCircles = () => {
-    canvasContext.clearRect(0, 0, canvasWidth, canvasHeight); // clear canvas
+    canvasContext.clearRect(0, 0, ViewState.canvasWidth, ViewState.canvasHeight); // clear canvas
     audioGraph.analyserNode.getByteFrequencyData(audioState.dataArray);
     canvasContext.fillStyle = `rgb(0,0,0)`;
-    canvasContext.fillRect(0, 0, canvasWidth, canvasHeight);
+    canvasContext.fillRect(0, 0, ViewState.canvasWidth, ViewState.canvasHeight);
     canvasContext.fillStyle = `rgb(255,255,255)`;
 
     let x = 0;
@@ -48,6 +48,6 @@ export const drawCircles = () => {
 
 
 // TO-IMP: 2023-10-02
-export const drawHorizontalBars = () => { canvasContext.clearRect(0, 0, canvasWidth, canvasHeight); }
-export const drawPD = () => { canvasContext.clearRect(0, 0, canvasWidth, canvasHeight);  }
-export const drawStars = () => { canvasContext.clearRect(0, 0, canvasWidth, canvasHeight); }
+export const drawHorizontalBars = () => { canvasContext.clearRect(0, 0, ViewState.canvasWidth, ViewState.canvasHeight); }
+export const drawPD = () => { canvasContext.clearRect(0, 0, ViewState.canvasWidth, ViewState.canvasHeight);  }
+export const drawStars = () => { canvasContext.clearRect(0, 0, ViewState.canvasWidth, ViewState.canvasHeight); }
