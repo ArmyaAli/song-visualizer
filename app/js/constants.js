c4b498a568f6f7b17cf4706041452b0d0d98b1f0
Ali Umar <ali.umar.work@gmail.com>
2023-10-09 09:00:26 -0400

    refactor into state.js for now

--- app/js/constants.js
+++
@@ -1,9 +0,0 @@
-import { drawVerticalBars, drawCircles, drawHorizontalBars, drawPD, drawStars } from './jobs.js'
-
-export const dispatchMap = {
-    "Vertical Bars": drawVerticalBars,
-    "Horizontal Bars": drawHorizontalBars,
-    "Circles": drawCircles,
-    "Possion distribution": drawPD,
-    "Stars": drawStars
-}
