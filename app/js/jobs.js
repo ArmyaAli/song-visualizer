85d8f0485814f03fab06b9075115c4b1a759952a
Ali Umar <ali.umar.work@gmail.com>
2023-10-21 15:16:06 -0400

    imp: star visualiization

--- app/js/jobs.js
+++ app/js/jobs.js
@@ -1,5 +1,6 @@
-import { canvasContext, ViewState, _DS_Fps } from "./state.js";
+import { canvasContext, ViewState, _DS_Fps, canvas } from "./state.js";
 import { audioGraph, audioState } from "./state.js";
+import { strokeStar } from "./util.js";
 
 export const drawVerticalBars = () => {
     canvasContext.clearRect(0, 0, ViewState.canvasWidth, ViewState.canvasHeight); // clear canvas
@@ -45,7 +46,7 @@ export const drawCircles = () => {
 
 }
 
-export const drawHorizontalBars = () => { 
+export const drawHorizontalBars = () => {
     canvasContext.clearRect(0, 0, ViewState.canvasWidth, ViewState.canvasHeight); // clear canvas
     audioGraph.analyserNode.getByteFrequencyData(audioState.dataArray);
     canvasContext.fillStyle = `rgb(0,0,0)`;
@@ -66,6 +67,50 @@ export const drawHorizontalBars = () => {
     _DS_Fps.count++;
 }
 
+export const drawStars = () => {
+    canvasContext.clearRect(0, 0, ViewState.canvasWidth, ViewState.canvasHeight); // clear canvas
+    audioGraph.analyserNode.getByteFrequencyData(audioState.dataArray);
+    canvasContext.fillStyle = `rgb(0,0,0)`;
+    canvasContext.fillRect(0, 0, ViewState.canvasWidth, ViewState.canvasHeight);
+    canvasContext.fillStyle = `rgb(255,255,255)`;
+    const height = ViewState.canvasHeight;
+    const width = ViewState.canvasWidth;
+    const cell = {
+        x: width / 8,
+        y: height / 8,
+    }
+    // Edges
+    let vertices = 5;
+    // Inset
+    let inset = 0.5;
+    // x position
+    let x = 64;
+    // y position
+    let y = 64;
+    // radius
+    let radius = 0;
+    // division constant
+    let di = 180;
+
+    let rowCount = 0;
+    let colCount = 0;
+
+
+    for (let i = 0; i < audioState.bufferLength; i++) {
+        radius += audioState.dataArray[i] / di;
+        canvasContext.fillStyle = `rgb(${radius + ViewState.color[0]}, ${ViewState.color[1]}, ${ViewState.color[2]})`;
+        strokeStar(x + cell.x * colCount, y + cell.y * rowCount, radius, vertices, inset);
+        if (colCount * cell.x < width) colCount++;
+        else {
+            if (rowCount * cell.y < height) {
+                rowCount++;
+                colCount = 0;
+            }
+        }
+        console.log(x, y, radius)
+    }
+
+    _DS_Fps.count++;
+}
 // TO-IMP: 2023-10-02
-export const drawPD = () => { canvasContext.clearRect(0, 0, ViewState.canvasWidth, ViewState.canvasHeight);  }
-export const drawStars = () => { canvasContext.clearRect(0, 0, ViewState.canvasWidth, ViewState.canvasHeight); }
+export const drawPD = () => { canvasContext.clearRect(0, 0, ViewState.canvasWidth, ViewState.canvasHeight); }
